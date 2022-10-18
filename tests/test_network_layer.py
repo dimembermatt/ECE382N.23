f3fa405cbed5801a6bb3cd6acc2ec1543f73c215
Allen Jiang <alljiang@hotmail.com>
2022-10-18 02:08:19 -0500

    change format

--- tests/test_network_layer.py
+++ tests/test_network_layer.py
@@ -3,13 +3,13 @@ import sys
 
 sys.path.append("../")
 
-from src.network_layer.message import Message
+from network_layer.network_layer import NetworkMessage
 
 if __name__ == "__main__":
     if int(sys.version[0]) < 3:
         raise Exception("This program only supports Python 3.")
 
-    message = Message("test", 4, bytearray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+    message = NetworkMessage("test", 4, bytearray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
 
     print(message)
 
