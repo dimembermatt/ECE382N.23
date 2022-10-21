6af9804a1e772e5966fc3a5bd20663a2e6073698
Allen Jiang <alljiang@hotmail.com>
2022-10-21 02:57:40 -0500

    create basic for network

--- tests/test_network_layer.py
+++ tests/test_network_layer.py
@@ -1,16 +1,29 @@
 
 import sys
 
-sys.path.append("../")
+sys.path.append("../src")
 
-from network_layer.network_layer import NetworkMessage
+from network_layer.network_layer import *
 
 if __name__ == "__main__":
     if int(sys.version[0]) < 3:
         raise Exception("This program only supports Python 3.")
 
-    message = NetworkMessage("test", 4, bytearray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+    layer = NetworkLayer()
 
-    print(message)
+    layer.add_device(0)
+    layer.add_device(1)
+
+    device_list = layer.get_device_list()
+    assert len(device_list) == 2
+
+    id_0 = device_list[0]
+    id_1 = device_list[1]
+
+    # length = 4, so 4 and 5 are ignored
+    message = NetworkMessage("test", 4, bytearray([0, 1, 2, 3, 4, 5]))
+    layer.schedule_transmission(id_0, id_1, message)
+    
+    layer.step_layer()
 
     
