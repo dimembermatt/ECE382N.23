c450bddb97b44ea670087cec7713a167c7bed015
Matthew Yu <matthewjkyu@gmail.com>
2022-11-26 21:50:24 -0600

    visualization of app layer

--- src/application_model/application_model_interface.py
+++ src/application_model/application_model_interface.py
@@ -6,10 +6,20 @@
 @date       2022-11-23
 """
 
+from datetime import date
+
+import matplotlib.pyplot as plt
+import numpy as np
+from colorhash import ColorHash
+
+
 class ApplicationModelInterface:
     def __init__(self) -> None:
         self._devices = {}
         self._event_timeline = []
+        self._fig, self._ax = plt.subplots()
+        self._ax.set_xlabel("Time (cycles)")
+        self._ax.set_ylabel("CPU")
 
     def add_device(self, device_name, device) -> bool:
         if device_name in self._devices:
@@ -21,8 +31,40 @@ class ApplicationModelInterface:
         return self._devices
 
     def generate_event_timeline(self) -> dict:
-        # Return event timeline
-        # TODO: implemented by concrete class
+        # Generate y components.
+        cores = []
+        for device_id, device in self._devices.items():
+            for core_id, core in device["cores"].items():
+                cores.append((f"{device_id}_{core_id}", device_id, core_id))
+
+        self._ax.set_ylim(0, len(cores) * 10)
+        self._ax.set_yticks([i * 10 + 5 for i in range(len(cores))])
+        self._ax.set_yticklabels([label for label, _, _ in cores])
+
+        # Plot events onto the timeline.
+        for event in self._event_timeline:
+            timestamp = int(event["timestamp"])
+            for device_id, device in event["devices"].items():
+                for core_id, core in device["cores"].items():
+                    idx = 0
+                    for idx_, core_ in enumerate(cores):
+                        if device_id == core_[1] and core_id == core_[2]:
+                            idx = idx_
+                    c = ColorHash(core).rgb
+                    self._ax.broken_barh(
+                        [(timestamp, 1)],
+                        (idx * 10 + 2, 6),
+                        color=(c[0] / 255, c[1] / 255, c[2] / 255),
+                    )
+                    self._ax.text(
+                        x=timestamp + 0.5,
+                        y=idx * 10 + 9,
+                        s=core,
+                        ha="center",
+                        va="center",
+                        color="black",
+                    )
+
         return self._event_timeline
 
     def get_event_timeline_step(self, step) -> (bool, dict):
@@ -31,3 +73,16 @@ class ApplicationModelInterface:
             return (False, {})
         else:
             return (True, self._event_timeline[step])
+
+    def print_event_timeline(self) -> None:
+        for event in self._event_timeline:
+            print(f"TIME: {event['timestamp']}")
+            print(f"Outputs generated: {event['cache']}")
+            print(f"Device usage:")
+            for device in event["devices"].items():
+                print(f"\t{device}")
+            print()
+
+    def visualize_event_timeline(self) -> None:
+        plt.tight_layout()
+        plt.show()
--- src/application_model/application_model_v0_0.py
+++ src/application_model/application_model_v0_0.py
@@ -1,7 +1,7 @@
 """_summary_
-@file       application_model_v0.py
+@file       application_model_v0_0.py
 @author     Matthew Yu (matthewjkyu@gmail.com)
-@brief      Models device applications on a logical time ordering.
+@brief      Models device applications.
 @version    0.0.0
 @date       2022-11-23
 """
@@ -15,31 +15,35 @@ from application_model_interface import ApplicationModelInterface
 
 class ApplicationModel_V0_0(ApplicationModelInterface):
     """_summary_
-    ApplicationModel_V0_0 models a very abstract interpretation of the NoS. It has
-    following characteristics:
+    ApplicationModel_V0_0 models a very abstract interpretation of the NoS. It
+    has the following characteristics:
     - timing is event driven and the model evaluates devices and device tasks
       when an event starts or finishes.
     - hardware is given a static idle and active energy consumption value, which
       is used for determining eneryg usage over time.
-    - transmission of data between devices is instantaneous and data is
-      represented as a single communication.
+    - all devices can talk to other devices in the network and transmission of
+      data between devices is instantaneous and data is represented as a single
+      communication.
     - a device has a static power source that provides a maximum fixed amount of
       energy at any moment.
 
-    NOTE: V1.1 (Application expansion 1) has the following characteristics:
+    NOTE: V0.1 (Application expansion 1) has the following characteristics:
     - device schedules are ordered by some fixed time resolution.
     - device tasks can take more than one cycle to execute.
-    - device cores have a core frequency
+    - device cores have a core frequency that affect execution time.
     - the model evaluates devices at each time step.
 
-    NOTE: V1.3 (Energy expansion 1) has the following characteristics:
+    NOTE: V0.2 (Energy expansion 1) has the following characteristics:
+    - multiple devices can share a single power source.
+    - energy consumption of CPU is based on the CPU utilization rate and
+      characteristics (frequency, supply voltage, current draw).
     - the power source has voltage and current characteristics within some
-      distribution
-    - multiple devices can share a single power source
+      distribution.
 
-    NOTE: V1.2 (Network expansion 1) has the following characteristics:
-    - devices have a physical location in space
-    - transmission of data is dependent based on distance between devices
+    NOTE: V0.3 (Network expansion 1) has the following characteristics:
+    - devices have a physical location in space.
+    - devices have a communication range that affects who they can talk to.
+    - transmission of data takes some period of time relative to the data type
     """
 
     def __init__(self) -> None:
@@ -48,7 +52,7 @@ class ApplicationModel_V0_0(ApplicationModelInterface):
     def add_device(self, device_name, device) -> bool:
         """_summary_
         Adds a device to the application model. A device must have the following
-        attributes: 
+        attributes:
         - device name (str)
         - a logical position consisting of a list of other devices that may or
           may not exist with which the device connects to (list(str))
@@ -96,12 +100,7 @@ class ApplicationModel_V0_0(ApplicationModelInterface):
         timestamp = 0
         while True:
             timeline_complete = True
-            timeline_entry = {
-                "timestamp": timestamp,
-                "devices": {
-                },
-                "cache": []
-            }
+            timeline_entry = {"timestamp": timestamp, "devices": {}, "cache": []}
 
             # Go through each device's schedule, look at the first task
             # If the first task has no remaining dependencies, pop from schedule
@@ -128,8 +127,12 @@ class ApplicationModel_V0_0(ApplicationModelInterface):
                             for dependency in task["dependencies"]:
                                 device["cache"].remove(dependency)
 
-                            timeline_entry["devices"][device_id]["cores"][core_id] = task["task_name"]
-                            timeline_entry["devices"][device_id]["hw"].extend(task["hw"])
+                            timeline_entry["devices"][device_id]["cores"][
+                                core_id
+                            ] = task["task_name"]
+                            timeline_entry["devices"][device_id]["hw"].extend(
+                                task["hw"]
+                            )
                             timeline_entry["cache"].append(task["outputs"])
                             del core[0]
 
@@ -149,63 +152,142 @@ class ApplicationModel_V0_0(ApplicationModelInterface):
             if timeline_complete:
                 break
 
-
         return super().generate_event_timeline()
+
 
 if __name__ == "__main__":
     if sys.version_info[0] < 3:
         raise Exception("This program only supports Python 3.")
     try:
         import pretty_traceback
+
         pretty_traceback.install()
     except ImportError:
-        pass    # no need to fail because of missing dev dependency
+        pass  # no need to fail because of missing dev dependency
 
     model = ApplicationModel_V0_0()
 
     device_0 = {
         "device_name": "device_0",
-        "neighbors": [],
         "cores": {
             "core_0": {
-                "active_power": 100, # Joules
-                "idle_power": 10, # Joules
+                "active_power": 100,  # Joules
+                "idle_power": 10,  # Joules
             }
         },
         "peripherals": {
             "comm_0": {
-                "active_power": 20, # Joules
-                "idle_power": 10, # Joules
+                "active_power": 20,  # Joules
+                "idle_power": 10,  # Joules
             },
             "adc_0": {
-                "active_power": 10, # Joules
-                "idle_power": 1, # Joules
-            }
+                "active_power": 10,  # Joules
+                "idle_power": 1,  # Joules
+            },
         },
         "schedule": {
             "core_0": [
                 {
-                    "task_name": "task_0a",
+                    "task_name": "task_A",
                     "dependencies": [],
                     "outputs": {
-                        "output_0a": ["device_0"],
+                        "output_0": ["device_0"],
                     },
-                    "hw": ["adc_0", ],
+                    "hw": [
+                        "adc_0",
+                    ],
                 },
                 {
-                    "task_name": "task_0b",
-                    "dependencies": ["output_0a"],
+                    "task_name": "task_B",
+                    "dependencies": ["output_0"],
                     "outputs": {
-                        "output_0b": ["device_0"],
+                        "output_1": ["device_1"],
                     },
-                    "hw": ["comm_0", ],
+                    "hw": [
+                        "comm_0",
+                    ],
                 },
             ]
         },
-        "cache": []
+        "cache": [],
+    }
+
+    device_1 = {
+        "device_name": "device_1",
+        "cores": {
+            "core_0": {
+                "active_power": 100,
+                "idle_power": 10,
+            },
+            "core_1": {
+                "active_power": 150,
+                "idle_power": 20,
+            },
+        },
+        "peripherals": {
+            "comm_0": {
+                "active_power": 20,  # Joules
+                "idle_power": 10,  # Joules
+            },
+            "adc_0": {
+                "active_power": 10,  # Joules
+                "idle_power": 1,  # Joules
+            },
+        },
+        "schedule": {
+            "core_0": [
+                {
+                    "task_name": "task_C",
+                    "dependencies": ["output_1"],
+                    "outputs": {
+                        "output_2": ["device_1"],
+                    },
+                    "hw": [
+                        "comm_0",
+                    ],
+                },
+                {
+                    "task_name": "task_D",
+                    "dependencies": ["output_5"],
+                    "outputs": {
+                        "output_6": ["device_1"],
+                    },
+                    "hw": [],
+                },
+            ],
+            "core_1": [
+                {
+                    "task_name": "task_E",
+                    "dependencies": ["output_2"],
+                    "outputs": {
+                        "output_3": ["device_1"],
+                    },
+                    "hw": [],
+                },
+                {
+                    "task_name": "task_F",
+                    "dependencies": ["output_3"],
+                    "outputs": {
+                        "output_4": ["device_1"],
+                    },
+                    "hw": [],
+                },
+                {
+                    "task_name": "task_G",
+                    "dependencies": [],
+                    "outputs": {
+                        "output_5": ["device_1"],
+                    },
+                    "hw": [],
+                },
+            ],
+        },
+        "cache": [],
     }
 
     model.add_device(device_0["device_name"], device_0)
-    event_timeline = model.generate_event_timeline()
-    pprint.pprint(event_timeline)
+    model.add_device(device_1["device_name"], device_1)
 
+    event_timeline = model.generate_event_timeline()
+    model.print_event_timeline()
+    model.visualize_event_timeline()
